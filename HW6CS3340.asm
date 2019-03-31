b80efee2c5c0df7c4200af0cae0a6e6a3505da71
Lathe Abusaid <labusaid@gmail.com>
2019-03-31 17:02:00 -0500

    Continued work on selection sort and reformatted

--- HW6CS3340.asm
+++ HW6CS3340.asm
@@ -50,6 +50,7 @@ main:
 #exit if num of bytes read <= 0
 #ble $v0, 0, error
 jal printArr
+printString(NewLine)
 jal selectionSortArr
 jal printArr
 
@@ -58,76 +59,95 @@ j exit
 #-----Functions-----#
 #Opens file and reads data into IntBuffer
 openFile:
-li   $v0, 13
-la   $a0, inFile
-la $a1, 0
-syscall
-#$s1 = File Descriptor
-move $s1, $v0
-li $v0, 14
-la $a1, IntBuffer
-syscall
-#set $a0 to address of filename
-move $a0, $s1
+    li   $v0, 13
+    la   $a0, inFile
+    la $a1, 0
+    syscall
+    #$s1 = File Descriptor
+    move $s1, $v0
+    li $v0, 14
+    la $a1, IntBuffer
+    syscall
+    #set $a0 to address of filename
+    move $a0, $s1
 jr $ra
 
 #Extracts integers from IntBuffer and inserts them into IntArr
 fillArr:
-la $a0, ($s1)
-li $t1, 48
-li $t2, 57
-la $a1, IntArr
-loop1:
-#load character
-addi $a0, $a0, 8
-lb $t0, ($a0)
-#test character
-beqz $t0, endLoop1
-blt $t0, $t1, loop1
-bgt $t0, $t2, loop1
-sw $t0, ($a1)
-addi $a1, $a1, 1
-j loop1
-endLoop1:
+    la $a0, ($s1)
+    li $t1, 48
+    li $t2, 57
+    la $a1, IntArr
+    loop1:
+    #load character
+    addi $a0, $a0, 8
+    lb $t0, ($a0)
+    #test character
+    beqz $t0, endLoop1
+    blt $t0, $t1, loop1
+    bgt $t0, $t2, loop1
+    sw $t0, ($a1)
+    addi $a1, $a1, 1
+    j loop1
+    endLoop1:
 jr $ra
 
 #Prints all ints in IntArr
 printArr:
-la $a1, IntArr
-lw $a0, 0($a1)
-loop2:
-li $v0, 1
-syscall
-printString(Space)
-#loads next element of IntArr into $a0
-addi $a1, $a1, 4
-lw $a0, 0($a1)
-#exit loop if IntArr element is 0
-bnez $a0, loop2
+    la $a1, IntArr
+    lw $a0, 0($a1)
+    loop2:
+    li $v0, 1
+    syscall
+    printString(Space)
+    #loads next element of IntArr into $a0
+    addi $a1, $a1, 4
+    lw $a0, 0($a1)
+    #exit loop if IntArr element is 0
+    bnez $a0, loop2
 jr $ra
 
 #Selection sorts IntArr
 selectionSortArr:
-la $a1, IntArr
-lw $t0, 0($a1)
-lw $t1, 4($a1)
-loop3:
-#$t0 is selected element, $t1 is compared element
-inLoop3:
-bge $t0, $t1, skip3
-sw $t0, 0($a2)
-sw $t1, 0($a1)
-skip3:
-addi $a2, $a2, 4
-lw $t1, 0($a2)
-bnez $t1, inLoop3
-#loads next elements of IntArr
-addi $a1, $a1, 4
-lw $t0, 0($a1)
-addi $a2, $a1, 4
-lw $t1, 0($a2)
-#exit loop if IntArr element is 0
-bnez $t0, loop3
+    la $a1, IntArr
+    lw $t0, 0($a1)
+    la $a2, IntArr
+    lw $t1, 0($a1)
+    la $a3, IntArr
+    lw $t2, 0($a1)
+    loop3:
+        #selects next elements of IntArr
+        addi $a1, $a1, 4
+        lw $t0, 0($a1)
+        move $a2, $a1
+        lw $t1, 0($a2)
+        addi $a3, $a1, 4
+        lw $t2, 0($a3)
+        
+        #$t0 is selected element, $t1 is compared element; $a1 and $a2 are their respective addresses
+        inLoop3:
+            #what this really needs in a do while loop to clean up the code
+            #load next element in $t1
+            addi $a2, $a2, 4
+            lw $t1, 0($a2)
+            beqz $t1 skip3 #if($t1 < $t2 && $t1! = 0)
+            bge $t1, $t2, skip3
+                move $t2, $t1 #set #t2 to value of $t1
+                move $a3, $a2 #set $a3 to address of $t1
+            skip3:
+        bnez $t1, inLoop3 #loop while $t1 != 0 (while not all elements have been compared)
+
+        #swap selected element with min found
+        sw $t0, 0($a3) #save $t0 to min address
+        sw $t3, 0($a1) #save $t3 to selected address
+
+        #debug
+        li $v0, 1
+        move $a0, $t0
+        syscall
+        printString(Space)
+
+    bnez $t0, loop3 #exit loop if  selected element is 0
 jr $ra
 
 #Calculates mean of IntArr
